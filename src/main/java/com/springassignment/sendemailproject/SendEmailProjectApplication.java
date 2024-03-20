1cf90683909cdc8485e50acb335615a3b86b2bb8
arjun292619 <j.c.arjunreddy@gmail.com>
2024-03-20 15:50:23 -0700

    Initial commit

---
+++ src/main/java/com/springassignment/sendemailproject/SendEmailProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springassignment.sendemailproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailProjectApplication.class, args);
+	}
+
+}
